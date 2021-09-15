27fae9fcb7cf889f858b67a11f457cd8dd5ae26d
Yasaman Ranjbar <Y.Ranjbar@mobtaker.local>
2021-09-15 15:22:48 +0430

    change my title article

--- pages/posts.vue
+++ pages/posts.vue
@@ -5,7 +5,7 @@
     <section class="hero is-info is-medium is-bold">
       <div class="hero-body">
         <div class="container has-text-centered">
-          <h1 class="title"> صفحه مقالات </h1>
+          <h1 class="title"> صفحه مقالات مربوط به وب سایت هینا </h1>
       </div>
       </div>
     </section>
